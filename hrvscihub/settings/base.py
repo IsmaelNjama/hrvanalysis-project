cf094e24c79885bd781c721124bbc625bec4c87e
IsmaelNjama <petermailsnjama@gmail.com>
2024-02-26 17:44:11 +0200

    solve staticfiles

--- hrvscihub/settings/base.py
+++ hrvscihub/settings/base.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ
 DEBUG = False
-
 ALLOWED_HOSTS = [
 	'localhost','127.0.0.1',
 ]
